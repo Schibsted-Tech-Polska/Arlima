6cefb76c2eab0ec3a014e5cde15c7cbbbfc25e82
Damian Rawski <damian.rawski@schibsted.pl>
2015-03-04 16:25:54 +0100

    Allowing use of relative paths for included files

--- classes/FileInclude.php
+++ classes/FileInclude.php
@@ -8,100 +8,122 @@
  */
 class Arlima_FileInclude {
 
-    /**
-     * @var bool|array
-     */
-    private static $current_file_args = false;
+	/**
+	 * @var bool|array
+	 */
+	private static $current_file_args = false;
 
-    /**
-     * @var bool
-     */
-    private static $is_collecting_args = false;
+	/**
+	 * @var bool
+	 */
+	private static $is_collecting_args = false;
 
-    /**
-     * @var array
-     */
-    private static $collected_args = array();
+	/**
+	 * @var array
+	 */
+	private static $collected_args = array();
 
-    /**
-     * @param string $file
-     * @return array
-     */
-    public function getFileArgs ($file)
-    {
-        self::$is_collecting_args = true;
-        $this->includeFile($file, array(), null, null);
-        self::$is_collecting_args = false;
-        return self::$collected_args;
-    }
+	/**
+	 * @param string $file
+	 * @return array
+	 */
+	public function getFileArgs ($file)
+	{
+		self::$is_collecting_args = true;
+		$this->includeFile($file, array(), null, null);
+		self::$is_collecting_args = false;
+		return self::$collected_args;
+	}
 
-    /**
-     * @return bool
-     */
-    public static function isCollectingArgs()
-    {
-        return self::$is_collecting_args;
-    }
+	/**
+	 * @return bool
+	 */
+	public static function isCollectingArgs()
+	{
+		return self::$is_collecting_args;
+	}
 
-    /**
-     * @param array $args
-     */
-    public static function setCollectedArgs($args)
-    {
-        self::$collected_args = $args;
-    }
+	/**
+	 * @param array $args
+	 */
+	public static function setCollectedArgs($args)
+	{
+		self::$collected_args = $args;
+	}
 
-    /**
-     * @return array|bool
-     */
-    public static function currentFileArgs()
-    {
-        return self::$current_file_args;
-    }
+	/**
+	 * @return array|bool
+	 */
+	public static function currentFileArgs()
+	{
+		return self::$current_file_args;
+	}
 
-    /**
-     * @param string $file
-     * @param array $args
-     * @param Arlima_AbstractListRenderingManager|null $renderer
-     * @param array|null $article
-     * @return string
-     */
-    public function includeFile($file, $args, $renderer=null, $article=null) // Last two arguments should be available in the included file
-    {
-        self::$current_file_args = $args;
+	/**
+	 * @param string $file
+	 * @param array $args
+	 * @param Arlima_AbstractListRenderingManager|null $renderer
+	 * @param array|null $article
+	 * @return string
+	 */
+	public function includeFile($file, $args, $renderer=null, $article=null) // Last two arguments should be available in the included file
+	{
+		self::$current_file_args = $args;
 
-        $cache_ttl = 0;
-        $cache_name = '';
+		$cache_ttl = 0;
+		$cache_name = '';
 
-        if( !self::$is_collecting_args ) {
-            $cache_ttl = apply_filters('arlima_file_include_cache_ttl', 0, $file);
-            if( $cache_ttl ) {
-                $cache_name = 'arlima_fileinc_'.basename($file).implode('_', $args);
-                $cached_content = Arlima_CacheManager::loadInstance()->get($cache_name);
-                if( $cached_content ) {
-                    if( $cached_content['expires'] < time() ) {
-                        Arlima_CacheManager::loadInstance()->delete($cache_name);
-                    } else {
-                        return $cached_content['content'];
-                    }
-                }
-            }
-        }
+		if( !self::$is_collecting_args ) {
+			$cache_ttl = apply_filters('arlima_file_include_cache_ttl', 0, $file);
+			if( $cache_ttl ) {
+				$cache_name = 'arlima_fileinc_'.basename($file).implode('_', $args);
+				$cached_content = Arlima_CacheManager::loadInstance()->get($cache_name);
+				if( $cached_content ) {
+					if( $cached_content['expires'] < time() ) {
+						Arlima_CacheManager::loadInstance()->delete($cache_name);
+					} else {
+						return $cached_content['content'];
+					}
+				}
+			}
+		}
 
-        // Include file and capture output
-        ob_start();
-        include $file;
-        $content = ob_get_contents();
-        ob_end_clean();
+		$content = '';
+		$resolved_path = $this->resolveFilePath($file, false);
 
-        self::$current_file_args = false;
+		if ($resolved_path) {
+			// Include file and capture output
+			ob_start();
+			include $file;
+			$content = ob_get_contents();
+			ob_end_clean();
 
-        if( $cache_ttl ) {
-            $content = "<!-- arlima file cache $cache_name ( ".date('Y-m-d H:i:s')." ttl: $cache_ttl )  -->\n".$content;
-            Arlima_CacheManager::loadInstance()->set($cache_name, array('expires' => time()+$cache_ttl, 'content'=>$content));
-        }
+			self::$current_file_args = false;
 
-        return $content;
-    }
+			if( $cache_ttl ) {
+				$content = "<!-- arlima file cache $cache_name ( ".date('Y-m-d H:i:s')." ttl: $cache_ttl )  -->\n".$content;
+				Arlima_CacheManager::loadInstance()->set($cache_name, array('expires' => time()+$cache_ttl, 'content'=>$content));
+			}
+		}
+
+		return $content;
+	}
+
+	/**
+	 * Returns the file path if it resides within the directory of the CMS.
+	 * @param string $path
+	 * @param bool $relative
+	 * @return bool|string
+	 */
+	function resolveFilePath($path, $relative=false)
+	{
+		$paths = explode(basename(WP_CONTENT_DIR), $path);
+		if( count($paths) > 1 ) {
+			$new_path = $paths[1];
+			if( file_exists(WP_CONTENT_DIR .'/'. $paths[1]) )
+				return $relative ? 'wp-content'.$new_path : rtrim(WP_CONTENT_DIR, '/') .'/'. $new_path;
+		}
+		return false;
+	}
 
 }
